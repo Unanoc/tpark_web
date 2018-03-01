ca537ec428c8fc4e0c6c48d89ac62d2277284e5f
Unanoc <danya4og@mail.ru>
2018-03-01 18:09:26 +0300

    added pagination and view-generator of fake data

--- question_ask_project/ask_me/urls.py
+++ question_ask_project/ask_me/urls.py
@@ -4,12 +4,14 @@ from ask_me.views import *
 # app_name = 'ask_me'
 urlpatterns = [
     url(r'^$', main),
-    url(r'^questions/$', questions, name='questions'),
+    url(r'^feed/$', feed, name='feed'),
+    url(r'^hot/$', hot, name='hot'),
+    url(r'^tag/bender/$', tag, name='tag'),
+
+    url(r'^question/(?P<question_id>[0-9]+)/$', question, name='question'),
     url(r'^login/$', login, name='login'),
     url(r'^registration/$', registration, name='registration'),
     url(r'^ask/$', new_ask, name='new_ask'),
-    url(r'^settings/$', settings, name='settings'),
-    url(r'^tag/bender/$', tag, name='tag'),
-    url(r'^question/1232343', question, name='question')
+    url(r'^settings/$', settings, name='settings')
 ]
 
--- question_ask_project/ask_me/views.py
+++ question_ask_project/ask_me/views.py
@@ -1,13 +1,101 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-# Create your views here.
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
 
 def main(request):
-	return redirect('questions')
+	return redirect('feed')
 
-def questions(request):
-	return HttpResponse(render_to_string('questions.html'))
+
+def feed(request):
+	questions = []
+	for i in range(1, 61):
+		questions.append({
+			'id': i,
+			'title': 'Вопрос №' + str(i),
+			'content': 'Now we might be able to get away with putting our templates directly in polls/templates (rather than creating another polls subdirectory), but it would actually be a bad idea. Django will choose the first template it finds whose name matches, and if you had a template with the same name in a different application, Django would be unable to distinguish between them. We need to be able to point Django at the right one, and the easiest way to ensure this is by namespacing them. That is, by putting those templates inside another directory named for the application itself.'
+		})
+
+	return render_paginator(request, questions,
+						   [{'title': 'New Questions',
+							 'url': 'feed',
+							 'class': 'new'},
+							{
+							 'title': 'Hot Questions',
+							 'url': 'hot',
+							 'class': 'hot'}])
+
+
+def hot(request):
+	questions = []
+	for i in range(1, 31):
+		questions.append({
+			'id': i,
+			'title': 'Вопрос №' + str(i),
+			'content': 'Examination of a sample of full texts of stories on disarmament with reference to the United Nations filed in May and August 1998 shows that only 10 per cent of stories covered substantive aspects of the work of the United Nations.'
+		})
+
+	return render_paginator(request, questions,
+						   [{'title': 'Hot Questions',
+							 'url': 'hot',
+							 'class': 'hot'},
+							{
+							 'title': 'New Questions',
+							 'url': 'feed',
+							 'class': 'new'}])
+
+
+def tag(request):
+	questions = []
+	for i in range(1, 10):
+		questions.append({
+			'id': i,
+			'title': 'Вопрос №' + str(i),
+			'content': 'Examination of a sample of full texts of stories on disarmament with reference to the United Nations filed in May and August 1998 shows that only 10 per cent of stories covered substantive aspects of the work of the United Nations.'
+		})
+
+	tag_name = 'bender'
+	return render_paginator(request, questions,
+						   [{'title': 'Tag: ' + tag_name,
+							 'url': 'tag',
+							 'class': 'tag-header'}])
+
+
+
+def render_paginator(request, question_list, headers):
+	paginator = Paginator(question_list, 20)  # Show 25 contacts per page
+
+	page = request.GET.get('page')
+
+	try:
+		questions = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		questions = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		questions = paginator.page(paginator.num_pages)
+
+	return render(request, 'feed.html',
+				  {'questions': questions,
+				   'headers': headers
+				   })
+
+
+
+
+
+
+
+
+def question(request, question_id):
+	question = {
+		'id': question_id,
+		'title': 'Вопрос №' + str(question_id),
+		'content': 'Examination of a sample of full texts of stories on disarmament with reference to the United Nations filed in May and August 1998 shows that only 10 per cent of stories covered substantive aspects of the work of the United Nations.'
+	}
+	return render(request, 'question.html', {'question': question})
 
 def login(request):
 	return HttpResponse(render_to_string('login.html'))
@@ -21,8 +109,4 @@ def new_ask(request):
 def settings(request):
 	return HttpResponse(render_to_string('settings.html'))
 
-def tag(request):
-	return HttpResponse(render_to_string('tag.html'))
 
-def question(request):
-	return HttpResponse(render_to_string('question.html'))
