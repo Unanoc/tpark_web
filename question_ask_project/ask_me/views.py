10e81fb8696b32da28adba5cc74b579042af822f
Unanoc <danya4og@mail.ru>
2018-03-23 21:11:34 +0300

    improved paginator, changed header design

--- question_ask_project/ask_me/views.py
+++ question_ask_project/ask_me/views.py
@@ -10,23 +10,38 @@ def main(request):
 
 
 def feed(request):
-	return render_paginator(request, question_list=Question.objects.get_new(), content_header=[
-		{'title': 'New Questions', 'url': 'feed', 'class': 'new'},
-        {'title': 'Hot Questions', 'url': 'hot', 'class': 'hot'}
-    ])
+	questions = render_paginator(request, objects_list=Question.objects.get_new())
+	content_header = [
+		{'title': 'New Questions', 'url': 'feed', 'is_active': True},
+		{'title': 'Hot Questions', 'url': 'hot', 'is_active': False}
+	]
+	return render(request, 'feed.html',
+                  {'questions': questions,
+				   'headers': content_header
+				   })
 
 
 def hot(request):
-	return render_paginator(request, question_list=Question.objects.get_hot(), content_header=[
-        {'title': 'Hot Questions', 'url': 'hot', 'class': 'hot'},
-        {'title': 'New Questions', 'url': 'feed', 'class': 'new'}
-    ])
+	questions = render_paginator(request, objects_list=Question.objects.get_hot())
+	content_header = [
+		{'title': 'New Questions', 'url': 'feed', 'is_active': False},
+		{'title': 'Hot Questions', 'url': 'hot', 'is_active': True}
+	]
+	return render(request, 'feed.html',
+				  {'questions': questions,
+				   'headers': content_header
+				   })
 
 
 def tag(request, tag):
-	return render_paginator(request, question_list=Tag.objects.get_by_tag(tag_name=tag), content_header=[
-        {'title': 'Tag: ' + tag, 'url': 'feed', 'class': 'tag-header'},
-    ])
+	questions = render_paginator(request, objects_list=Tag.objects.get_by_tag(tag_name=tag))
+	content_header = [
+		{'title': 'Tag: ' + tag, 'url': 'feed', 'is_active': True},
+	]
+	return render(request, 'feed.html',
+				  {'questions': questions,
+				   'headers': content_header
+				   })
 
 
 def question(request, question_id):
@@ -37,8 +52,21 @@ def question(request, question_id):
 		raise Http404
 
 
+def render_paginator(request, objects_list):
+	paginator = Paginator(objects_list, 20)  # Show 20 contacts per page
 
+	page = request.GET.get('page')
 
+	try:
+		objects = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		objects = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		objects = paginator.page(paginator.num_pages)
+
+	return objects
 
 
 
@@ -63,26 +91,3 @@ def settings(request):
 
 
 
-
-
-
-
-
-def render_paginator(request, question_list, content_header = ''):
-	paginator = Paginator(question_list, 20)  # Show 25 contacts per page
-
-	page = request.GET.get('page')
-
-	try:
-		questions = paginator.page(page)
-	except PageNotAnInteger:
-		# If page is not an integer, deliver first page.
-		questions = paginator.page(1)
-	except EmptyPage:
-		# If page is out of range (e.g. 9999), deliver last page of results.
-		questions = paginator.page(paginator.num_pages)
-
-	return render(request, 'feed.html',
-                  {'questions': questions,
-				   'headers': content_header
-				   })
