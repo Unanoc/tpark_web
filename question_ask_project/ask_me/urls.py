2dd62bbf2b2922305b819bd89cab0f440e0d5d66
Unanoc <danya4og@mail.ru>
2018-03-23 17:41:00 +0300

    little changed

--- question_ask_project/ask_me/urls.py
+++ question_ask_project/ask_me/urls.py
@@ -8,10 +8,11 @@ urlpatterns = [
     url(r'^feed/$', feed, name='feed'),
     url(r'^hot/$', hot, name='hot'),
     url(r'^tag/(?P<tag>.*)/$', tag, name='tag'),
-
     url(r'^question/(?P<question_id>[0-9]+)/$', question, name='question'),
+
     url(r'^ask/$', new_ask, name='new_ask'),
     url(r'^settings/$', settings, name='settings'),
+
     url(r'^login/$', login, name='login'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^registration/$', registration, name='registration')
